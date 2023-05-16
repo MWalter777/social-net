2178db48a6bf660e4fcc0cccbd2f5f8daa41dbce
Walter Marroquin <waltermarrom777@gmail.com>
2023-05-16 15:46:22 -0600

    add lorem text

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -3,9 +3,15 @@ import UserHeader from './UserHeader';
 
 const Post = () => {
 	return (
-		<div className='w-full bg-white text-primary flex flex-col p-2 rounded-sm shadow-lg'>
+		<div className='w-full bg-white text-primary flex flex-col p-2 gap-2 rounded-sm shadow-lg'>
 			<UserHeader />
-			<div>askjhdfkj</div>
+			<div className='text-gray-700'>
+				<p className='text-justify px-1 text-sm md:text-base'>
+					Nostrud sit minim aliqua pariatur do in nisi eu nulla eu nisi. Ipsum
+					commodo amet dolor elit non esse. Aliqua ad dolor do exercitation
+					nostrud nulla.
+				</p>
+			</div>
 		</div>
 	);
 };
