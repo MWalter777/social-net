298020755a887d0820c2badfffee282de7036a49
Walter Marroquin <waltermarrom777@gmail.com>
2023-05-16 17:06:43 -0600

    show images

--- src/components/posts/PostContent.tsx
+++ src/components/posts/PostContent.tsx
@@ -1,4 +1,8 @@
+import Image from 'next/image';
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import LogoFull from '@/assets/logo-full.png';
+import Logo from '@/assets/logo.png';
 
 const PostContent = () => {
 	return (
@@ -6,58 +10,23 @@ const PostContent = () => {
 			<p className='text-justify px-1 text-sm md:text-base truncate-3'>
 				Elit ipsum id elit cupidatat elit sunt adipisicing nisi esse commodo.
 				Deserunt esse magna eu nisi reprehenderit mollit reprehenderit eu magna
-				culpa deserunt sit voluptate. Et ea non pariatur aute irure voluptate
-				exercitation sint culpa. Labore consectetur Lorem dolore quis anim do
-				esse consequat ad. Sint anim tempor veniam laborum reprehenderit ea
-				consectetur. Officia sunt et do dolore proident ut ullamco fugiat est
-				dolor. Exercitation cillum amet et occaecat aliquip amet elit ea
-				officia. Magna exercitation nulla esse aute dolore tempor commodo irure
-				velit ullamco. Incididunt ea laboris consequat aute sit culpa ipsum
-				magna reprehenderit laboris consectetur culpa velit sit. Occaecat
-				exercitation voluptate non tempor elit et mollit. Sunt laboris culpa
-				nisi officia ut quis officia. Labore Lorem veniam sint voluptate dolor
-				irure ea excepteur cupidatat nostrud incididunt id. Veniam dolore ea
-				veniam ipsum est nisi sunt est ad laborum eiusmod. Et aliqua enim
-				cupidatat ut ullamco. Non voluptate nostrud voluptate magna labore ex
-				laborum nisi. Deserunt qui minim adipisicing amet ut non esse magna sint
-				magna culpa. Duis ipsum veniam minim pariatur dolor ex occaecat. Laborum
-				dolore proident ad reprehenderit nulla ea. Quis proident ea nulla in ad
-				dolor aliqua. Dolor do pariatur in amet labore voluptate dolore nostrud
-				laboris magna duis pariatur sunt minim. Cupidatat consequat laboris
-				voluptate incididunt voluptate sunt exercitation minim. Ex excepteur
-				veniam nisi sint sit fugiat sunt cillum eiusmod consectetur. Consequat
-				fugiat voluptate reprehenderit commodo magna ut. Anim quis officia
-				incididunt consectetur reprehenderit et consequat velit consequat
-				consequat labore. Ex sit Lorem voluptate non est. Laboris ea excepteur
-				minim esse qui aliqua eu minim sunt tempor fugiat enim ut. Proident sunt
-				nostrud elit aliquip sit mollit amet nostrud reprehenderit cillum.
-				Laboris culpa voluptate nisi proident id aute exercitation reprehenderit
-				sit deserunt velit voluptate. Fugiat consequat enim irure in consequat.
-				Nisi ea commodo consequat aute. Laborum do fugiat deserunt irure nulla
-				amet cupidatat occaecat cupidatat reprehenderit. Elit consectetur non
-				consequat nostrud occaecat dolor non amet proident. Fugiat reprehenderit
-				ex occaecat minim cillum commodo aliqua aute. Consequat eu sint dolore
-				sunt reprehenderit sit. Tempor esse in velit officia elit in magna
-				cupidatat amet. Fugiat ipsum in culpa culpa mollit. Excepteur irure
-				deserunt minim minim irure veniam quis eiusmod commodo sint occaecat id
-				excepteur. Incididunt dolor sunt laborum incididunt reprehenderit est
-				occaecat proident consectetur. Commodo cupidatat magna veniam consequat
-				sunt enim incididunt voluptate sit eu. Officia fugiat deserunt ipsum
-				mollit pariatur anim nisi exercitation. Commodo in proident dolor dolor
-				ullamco. Exercitation aliquip aliquip velit sunt ex dolore nostrud
-				adipisicing fugiat veniam ut aliqua aliqua. Duis et dolore ipsum est
-				duis aliquip excepteur mollit do voluptate tempor eu. Non mollit laborum
-				fugiat non eiusmod dolor nisi ea incididunt irure aute officia elit.
-				Reprehenderit nisi sit veniam tempor nisi incididunt ad cillum aliqua.
-				Ad et do do consectetur do velit velit occaecat ea irure nulla amet
-				incididunt. In cillum in esse elit quis amet ipsum ullamco esse. Non
-				officia anim anim nostrud cillum. Aliquip consequat veniam fugiat aute
-				sit esse ad nisi consectetur esse. Pariatur elit sit deserunt ad magna.
-				Ad dolor mollit deserunt fugiat aliquip labore officia amet est
-				incididunt aute. In ex fugiat proident sint ut pariatur. Laboris enim
-				dolor magna velit aliquip ipsum aute cillum laborum minim dolore laborum
-				sit.
 			</p>
+			<div>
+				<Carousel>
+					<div>
+						<Image src={Logo} alt='logo' />
+						<p className='legend'>Legend 1</p>
+					</div>
+					<div>
+						<Image src={LogoFull} alt='logo' />
+						<p className='legend'>Legend 2</p>
+					</div>
+					<div>
+						<Image src={Logo} alt='logo' />
+						<p className='legend'>Legend 3</p>
+					</div>
+				</Carousel>
+			</div>
 		</div>
 	);
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@/styles/spinners.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { SessionProvider } from 'next-auth/react';
 
 import type { AppProps } from 'next/app';
