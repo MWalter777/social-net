907a69b0127ef19a2627b8e571ead23e8902227a
Walter Marroquin <waltermarrom777@gmail.com>
2023-05-16 15:38:31 -0600

    add userHeader

--- src/components/posts/UserHeader.tsx
+++ src/components/posts/UserHeader.tsx
@@ -1,7 +1,107 @@
-import React from 'react';
+import {
+	Avatar,
+	IconButton,
+	Menu,
+	MenuItem,
+	Tooltip,
+	Typography,
+} from '@mui/material';
+import React, { MouseEvent, useState } from 'react';
+import { IconType } from 'react-icons';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { MdAddBox, MdMessage, MdReportProblem } from 'react-icons/md';
+
+type Setting = {
+	id: number;
+	name: string;
+	Icon: IconType;
+	onClick?: () => void;
+};
 
 const UserHeader = () => {
-	return <div>UserHeader</div>;
+	const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+	const settings: Setting[] = [
+		{
+			id: 1,
+			name: 'Add user',
+			Icon: MdAddBox,
+			onClick: () => {
+				console.log('MdAddBox');
+			},
+		},
+		{
+			id: 2,
+			name: 'Message',
+			Icon: MdMessage,
+			onClick: () => {
+				console.log('MdMessage');
+			},
+		},
+		{
+			id: 3,
+			name: 'Report user',
+			Icon: MdReportProblem,
+			onClick: () => {
+				console.log('report user');
+			},
+		},
+	];
+
+	const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+	return (
+		<div className='flex justify-between items-center border border-red-400'>
+			<div className='flex gap-4 items-center border border-green-500'>
+				<Avatar src={''} />
+				<div className='flex flex-col justify-between'>
+					<p className='font-bold'>username</p>
+					<p className='text-xs'>@username</p>
+				</div>
+			</div>
+			<div>
+				<Tooltip title='Open settings'>
+					<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+						<BsThreeDotsVertical />
+					</IconButton>
+				</Tooltip>
+				<Menu
+					sx={{ mt: '45px' }}
+					id='menu-appbar'
+					anchorEl={anchorElUser}
+					anchorOrigin={{
+						vertical: 'top',
+						horizontal: 'right',
+					}}
+					keepMounted
+					transformOrigin={{
+						vertical: 'top',
+						horizontal: 'right',
+					}}
+					open={Boolean(anchorElUser)}
+					onClose={handleCloseUserMenu}
+				>
+					{settings.map((setting) => (
+						<MenuItem
+							key={setting.id}
+							onClick={handleCloseUserMenu}
+							className='hover-primary-menu hover:text-white flex gap-2'
+						>
+							<setting.Icon />
+							<Typography textAlign='center' onClick={setting.onClick}>
+								{setting.name}
+							</Typography>
+						</MenuItem>
+					))}
+				</Menu>
+			</div>
+		</div>
+	);
 };
 
 export default UserHeader;
