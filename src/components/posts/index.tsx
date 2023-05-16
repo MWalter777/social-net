9517a143d35b47ec499be7f3c161ddf42d568679
Walter Marroquin <waltermarrom777@gmail.com>
2023-05-16 17:21:58 -0600

    show specific post

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -5,7 +5,7 @@ import PostActions from './PostActions';
 
 const Post = () => {
 	return (
-		<div className='w-full md:w-10/12 lg:w-8/12 bg-white text-primary flex flex-col p-2 gap-2 rounded-sm shadow-lg'>
+		<div className='w-full md:w-10/12 lg:w-8/12 bg-white text-primary flex flex-col p-2 md:p-4 lg:p-8 gap-2 rounded-sm shadow-lg'>
 			<UserHeader />
 			<PostContent />
 			<PostActions />
---
+++ src/pages/p/[id].tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/layout';
+import React from 'react';
+
+const Post = () => {
+	return (
+		<Layout>
+			<div className='w-full flex flex-col gap-1 items-center justify-center'>
+				<Post />
+			</div>
+		</Layout>
+	);
+};
+
+export default Post;
